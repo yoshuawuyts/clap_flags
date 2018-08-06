0790cb6573abe94e40e362a257791af60d05a000
Yoshua Wuyts <yoshuawuyts@gmail.com>
2018-08-06 21:34:51 +0200

    add permission

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,11 @@
 #![cfg_attr(test, deny(warnings))]
 
 extern crate clap_log_flag;
+extern crate clap_permission_flag;
 extern crate clap_port_flag;
 extern crate clap_verbosity_flag;
 
 pub use clap_log_flag::Log;
+pub use clap_permission_flag::Permission;
 pub use clap_port_flag::Port;
 pub use clap_verbosity_flag::Verbosity;
